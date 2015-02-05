d327993f54536154a9a3a465471c950f359aa42e
dqgorelick <dqgorelick@gmail.com>
2015-02-05 11:09:40 -0500

    **ONLY MAKE CHANGES TO TESTING FOLDER**

--- js/main.js
+++ js/main.js
@@ -7,11 +7,15 @@ $(document).ready(function() {
 });
 
 function stars(event){
-	var scrollPos = $(document).scrollTop();
-	var scrollPosHalf = scrollPos/2;
-	var scrollPosQuarter = scrollPos/4;
-	var bodyWidth = $("body").width();
-	var stars = $(".stars");
+	var scrollPos = $(document).scrollTop()
+	var scrollPosHalf = scrollPos/2
+	var scrollPosQuarter = scrollPos/4
+	var bodyWidth = $("body").width()
+	var bodyHeight = 2000
+	var stars = $(".stars")
+	var video = $(".video")
+	// stars.css("top",scrollPosQuarter).css("height",bodyHeight-scrollPosQuarter)
+
 	if ( bodyWidth > 730){
 		var bodyHeight = $(".desktop").height();
 		stars.css("top",scrollPosQuarter).css("height",bodyHeight-scrollPosQuarter);
